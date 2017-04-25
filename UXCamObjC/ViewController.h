e08698db3d9e535e8be88ab0a1e1166b97a233d0
Mehul DV <montydavid05@gmail.com>
2017-04-25 11:02:59 -0400

    UXCam Objective C
    Fix User Experience Issues by recording and analyzing what users do
    inside your app

---
+++ UXCamObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UXCamObjC
+//
+//  Created by mehul patel on 2017-04-25.
+//  Copyright © 2017 mehul patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UXCamObjC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UXCamObjC
+//
+//  Created by mehul patel on 2017-04-25.
+//  Copyright © 2017 mehul patel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
